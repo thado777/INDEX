3d5098a174d235b47d69931992898141421402ed
thado777 <coramdeq@gmail.com>
2024-09-28 09:19:57 +0900

    데이터 파일

--- tensor.py
+++ tensor.py
@@ -1,1 +1,7 @@
-import tensorflow as tf
+import yfinance as yf
+
+data = yf.download('SPY QQQ', start = '2010-01-01')
+
+data['Adj Close'].rebase(1).plot()
+
+#[출처] [소개] 국내장 주가정보와 재무정보를 가져올 수 있는 파이썬 라이브러리 (FinanceDataReader, OpenDartReader)|작성자 오렌지사과
