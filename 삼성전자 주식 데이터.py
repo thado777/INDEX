cb55e75259342b1d691e2fbd114a5a1d824dbfd3
thado777 <coramdeq@gmail.com>
2024-10-19 09:10:46 +0900

    second

--- 삼성전자 주식 데이터.py
+++ 삼성전자 주식 데이터.py
@@ -1,5 +1,5 @@
 import yfinance as yf
 
 # 예시: 삼성전자 주식 데이터를 불러오기
-stock_data = yf.download('005930.KS', start='2020-01-01', end='2024-10-19')
+stock_data = yf.download('005930.KS', start='2020-01-01', end='2023-01-01')
 stock_data.to_csv('samsung_stock.csv')  # CSV로 저장
